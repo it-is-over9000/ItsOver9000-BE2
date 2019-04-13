36bfe266bfaea5ff17dd8020ad9c89079be668ac
Joshua Wood <JerichoWall@apu.edu>
2019-04-13 12:51:17 -0700

    Basic framework complete

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+
+module.exports = server;
