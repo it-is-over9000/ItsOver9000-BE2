5d6706373cf5dae5b25d5fa8ea2a2a92c9cee0e3
Joshua Wood <JerichoWall@apu.edu>
2019-04-17 14:01:10 -0700

    Fixed password hash for seed data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,16 +1,19 @@
+const bcrypt = require('bcryptjs');
+
+const hash = bcrypt.hashSync('pass', 6);
 
 exports.seed = function(knex) {
   
       return knex('users').insert([
-        {id: 1, username: 'Rudy', password: 'pass'},
-        {id: 2, username: 'Alejandro', password: 'pass'},
-        {id: 3, username: 'Crawford', password: 'pass'},
-        {id: 4, username: 'Winston', password: 'pass'},
-        {id: 5, username: 'Arthur', password: 'pass'},
-        {id: 6, username: 'Joshua', password: 'pass'},
-        {id: 7, username: 'Josh', password: 'pass'},
-        {id: 8, username: 'Krisli', password: 'pass'},
-        {id: 9, username: 'Ken', password: 'pass'}
+        {id: 1, username: 'Rudy', password: hash},
+        {id: 2, username: 'Alejandro', password: hash},
+        {id: 3, username: 'Crawford', password: hash},
+        {id: 4, username: 'Winston', password: hash},
+        {id: 5, username: 'Arthur', password: hash},
+        {id: 6, username: 'Joshua', password: hash},
+        {id: 7, username: 'Josh', password: hash},
+        {id: 8, username: 'Krisli', password: hash},
+        {id: 9, username: 'Ken', password: hash}
       ]);
     
 };
