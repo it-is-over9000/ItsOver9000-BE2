b7960601f4d077a47fe9c6c4a08c6cacf5ec8c59
Joshua Wood <JerichoWall@apu.edu>
2019-04-18 12:15:10 -0700

    Altered dummy data

--- db2018/db2018-router.js
+++ db2018/db2018-router.js
@@ -3,22 +3,22 @@ const Games = require('./db2018-model.js');
 
 router.post('/', (req, res) => {
     
-    let dummyArray = [];
+    // let dummyArray = [];
 
-    for(let i = 1; i <= 30; i++) {
-        dummyArray.push(Math.round((Math.random() * 181)));
-    }
+    // for(let i = 1; i <= 30; i++) {
+    //     dummyArray.push(Math.round((Math.random() * 181)));
+    // }
     
-    let dummyOutlook = 0;
+    let dummyOutlook = Math.round((Math.random() * 181));
 
-    for(let i = 0; i < dummyArray.length; i++) {
-        dummyOutlook = dummyOutlook + dummyArray[i];
-    }
-    console.log(dummyArray);
-    dummyOutlook = Math.round(dummyOutlook/30);
+    // for(let i = 0; i < dummyArray.length; i++) {
+    //     dummyOutlook = dummyOutlook + dummyArray[i];
+    // }
+    // console.log(dummyArray);
+    // dummyOutlook = Math.round(dummyOutlook/30);
     console.log('Dummy Outlook', dummyOutlook)
 
-    let response = ({dummyArray, dummyOutlook});
+    let response = ({dummyOutlook});
     
     res.status(200).json(response);
 })
